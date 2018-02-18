57761bd89f06d70e93859878c9276e55ddcbd00c
Diego Borrero <die.9511@gmail.com>
2018-02-18 09:19:09 -0500

    First Commit

---
+++ Cover Template for Bootstrap_files/OrdersController.js
@@ -0,0 +1,23 @@
+var order1 = {
+	"order_id": 1,
+	"table_id": 1,
+	"products": [{
+      "product":"PIZZA",
+      "quantity":3,
+      "price":"$10000"
+    },
+		{
+			"product": "HOTDOG",
+			"quantity": 1,
+			"price": "$3000"
+		},
+    {
+			"product": "COKE",
+			"quantity": 4,
+			"price": "$1300"
+		}
+	]
+};
+function addNewOrder(){
+
+}
