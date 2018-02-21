8e8573ecd021fbe9155ad4a3b5800e7290273429
unknown <2116177@labinfo.is.escuelaing.edu.co>
2018-02-21 13:26:23 -0500

    Funcion de agregar tabla hecha. falta redireccionamiento a su ruta

--- Cover Template for Bootstrap_files/OrdersController.js
+++ Cover Template for Bootstrap_files/OrdersController.js
@@ -1,6 +1,5 @@
 var order1 = {
-	"order_id": 1,
-	"table_id": 1,
+	"table_id": 2,
 	"products": [{
       "product":"PIZZA",
       "quantity":3,
@@ -18,6 +17,36 @@ var order1 = {
 		}
 	]
 };
-function addNewOrder(){
-
+function addNewOrder() {
+	
+	var tope = new Array();
+	tope.push("Product");
+	tope.push("Quantity");
+	tope.push("Price");
+ 
+    var table = document.createElement("TABLE");
+    table.border = "1";
+ 
+    var columnCount = 3;
+ 
+    var row = table.insertRow(-1);
+    for (var i = 0; i < columnCount; i++) {
+        var headerCell = document.createElement("TH");
+        headerCell.innerHTML = tope[i];
+        row.appendChild(headerCell);
+    }
+ 
+    for (var i = 0; i < order1.products.length; i++) {
+        row = table.insertRow(-1);
+        var cell = row.insertCell(-1);
+        cell.innerHTML = order1.products[i].product;
+		var cell = row.insertCell(-1);
+        cell.innerHTML = order1.products[i].quantity;
+		var cell = row.insertCell(-1);
+        cell.innerHTML = order1.products[i].price;
+        }
+ 
+    var dvTable = document.getElementById("dvTable");
+    dvTable.innerHTML = "";
+    dvTable.appendChild(table);
 }
